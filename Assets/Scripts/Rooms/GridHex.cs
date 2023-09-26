c812b0cb2ea2ee2cfde476858b41bf9abae47fab
Alena Klimecká <alena.klimecka@gmail.com>
2023-09-26 21:35:33 +0200

    Final procedural generation

--- Assets/Scripts/Rooms/GridHex.cs
+++ Assets/Scripts/Rooms/GridHex.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -14,10 +15,11 @@ public class GridHex : MonoBehaviour
     public int width;
     public int height;
     public Vector3 offset;
-    public float noiseLevel;
+    public float noiseLimit;
     public float noiseScale;
 
     public GameObject hexEmpty;
+
 
 
 
@@ -27,31 +29,41 @@ public class GridHex : MonoBehaviour
         CreateAllHexCenter();
     }
 
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(2))
+        {
+            DeleteRooms();
+            CreateAllHexCenter();
+        }
+    }
+
     public void CreateAllHexCenter()
     {
-        float[,] noiseMap = new float[width, height];
-        float xOffset = UnityEngine.Random.Range(-10000f, 10000f);
-        float yOffset = UnityEngine.Random.Range(-10000f, 10000f);
 
         int hexSize = (int)grid.cellSize.x;
+        int xCenter = width / 2;
+        int yCenter = height / 2;
         hexagons = new Vector3[width, height];
 
-        for (int y = 0; y < height; y++)
-        {
-            for (int x = 0; x < width; x++)
-            {
-                float noiseValue = Mathf.PerlinNoise(x * noiseScale + xOffset, y * noiseScale + yOffset);
-                noiseMap[x, y] = noiseValue;
-            }
-        }
+        float[,] noiseMap = CreatePerlinNoise();
 
         for (int y = 0; y < height; y++)
         {
+            // spocita pravdepodobnost vygenerovani policka pro y
+            float yDistance = Math.Abs(y - yCenter);
+            float yPercentage = 1 - (yDistance / yCenter);
+
+
             for (int x = 0; x < width; x++)
             {
-                float noiseValue = noiseMap[x, y];
+                // spocita pravdepodobnost vygenerovani policka pro x
+                float xDistance = Math.Abs(x - xCenter);
+                float xPercentage = 1 - (xDistance / xCenter);
 
-                if (noiseValue > noiseLevel)
+                float noiseValue = noiseMap[x, y] + xPercentage + yPercentage;
+
+                if (noiseValue > noiseLimit)
                 {
                     Vector3 centrePosition = HexMath.Center(hexSize, x, y) + offset;
                     hexagons[x, y] = centrePosition;
@@ -62,11 +74,38 @@ public class GridHex : MonoBehaviour
                 {
                     hexagons[x, y] = new Vector3(0, 0, -99);
                 }
-                
 
             }
         }
 
+    }
+
+    public float[,] CreatePerlinNoise()
+    {
+        float[,] noiseMap = new float[width, height];
+        float xOffset = UnityEngine.Random.Range(-10000f, 10000f);
+        float yOffset = UnityEngine.Random.Range(-10000f, 10000f);
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                float noiseValue = Mathf.PerlinNoise(x * noiseScale + xOffset, y * noiseScale + yOffset);
+                noiseMap[x, y] = noiseValue;
+            }
+        }
+
+        return noiseMap;
+    }
+
+    public void DeleteRooms()
+    {
+        GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("EmptyRoom");
+
+        foreach (GameObject go in gameObjects)
+        {
+            Destroy(go);
+        }
     }
 
 }
