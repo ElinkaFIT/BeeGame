c9e1e7b84afcaff2dafa27ab6e292f8eddae1dee
Alena Klimecká <alena.klimecka@gmail.com>
2024-04-20 23:10:14 +0200

    stop duplicating logs

--- Assets/_Scripts_/UI/Log.cs
+++ Assets/_Scripts_/UI/Log.cs
@@ -8,14 +8,24 @@ public class Log : MonoBehaviour
     public static Log instance;
     public GameObject logContentPrefab;
 
+    private float lastLogTime;
+    private string lastLogName;
+
     void Start()
     {
         instance = this;
+        lastLogTime = Time.time;
+        lastLogName = "startLog";
         AddNewLogText(Time.time, "Game start", Color.black);
     }
 
     public void AddNewLogText(float logTime, string text, Color textColor)
     {
+        if (IsItDuplicateLog(logTime, text))
+        {
+            return;
+        }
+
         // generate new text
         Vector3 spawnPosition = new Vector3(0, 0, 0);
         GameObject newLog = Instantiate(logContentPrefab, spawnPosition, Quaternion.identity);
@@ -30,6 +40,19 @@ public class Log : MonoBehaviour
         int minute = ((int)logTime / 60);
         int second = ((int)logTime % 60);
         newText.text = string.Format("{0:00}:{1:00}", minute, second) + " " + text;
+    }
+
+    private bool IsItDuplicateLog(float logTime, string text)
+    {
+        if(text == lastLogName && logTime - lastLogTime < 1)
+        {
+            lastLogTime = logTime;
+            lastLogName = text;
+            return true;
+        }
+        lastLogTime = logTime;
+        lastLogName = text;
+        return false;
     }
     
 }
